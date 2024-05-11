448e605650da1d5f6678c16b0e2e7929a0118fe9
Felipenamu <felipenamu@gmail.com>
2024-05-11 09:27:04 +0200

    Star project

---
+++ src/ED_T6_RA04/Main.java
@@ -0,0 +1,13 @@
+package ED_T6_RA04;
+
+/**
+ *
+ * @author felip
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Tarea6EDClase miCarrito = new Tarea6EDClase();
+        miCarrito.aplicarDescuento(100, 5);
+    }
+}
